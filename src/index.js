fb9867ae8de7822d46df2229cb6470cf98eec50d
Max Zhao <maxzhao98@gmail.com>
2022-05-19 11:55:12 -0500

    Initialize react app and file structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+function App() {
+  return <div>hello world</div>;
+}
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App name="Shopify-ai" />, mountNode);
