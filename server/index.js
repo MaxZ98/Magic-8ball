234e04543f927de93d28529a02d0154427b3c773
Max Zhao <maxzhao98@gmail.com>
2022-05-22 23:03:37 -0500

    moved api key to env file

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
+require("dotenv").config();
 const axios = require("axios");
 const cors = require("cors");
-//const { API_KEY, AI_Template } = require("../config.js");
-const KEY = process.env.API_KEY;
-const Template = process.env.AI_Template;
 const { Configuration, OpenAIApi } = require("openai");
 const app = express();
 const PORT = 3000;
+const key = process.env.API_KEY;
 
 app.use(express.static(__dirname + "/../dist"));
 app.use(express.json());
@@ -17,12 +16,13 @@ app.listen(PORT, () => {
 });
 
 app.post("/completionRequest", async (req, res) => {
+  const template = (AI_Template =
+    "Q: should i ask my crush on a date?/nA: no, what is there to like about you/n/nQ: am i attractive?/nA: no, deal with it/n/nQ: does my crush think im cute?/nA: no she actually thinks you are ugly/n/nQ: do my friends like me?/nA: yes, they think you are awesome/n/nQ: am i a cool person?/nA: no, you are extremely lame/n/nQ: do people care about me?/nA: definitely not/n/n");
   const inputText = req.body.params;
-  const finalPrompt = Template + "Q: " + inputText;
-  //console.log(finalPrompt);
+  const finalPrompt = template + "Q: " + inputText;
 
   const configuration = new Configuration({
-    apiKey: KEY,
+    apiKey: key,
   });
   const openai = new OpenAIApi(configuration);
   const response = await openai.createCompletion("text-curie-001", {
@@ -30,8 +30,6 @@ app.post("/completionRequest", async (req, res) => {
     max_tokens: 100,
     temperature: 0.5,
     n: 1,
-    //echo: true,
   });
   res.status(200).send(response.data.choices[0].text);
-  //console.log(response.data.choices[0].text);
 });
