d0feff0123466c81befdcd0896bb7314140ca597
Max Zhao <maxzhao98@gmail.com>
2022-05-22 19:28:21 -0500

    updated files to respect gitignore

--- config.js
+++
@@ -1,5 +0,0 @@
-API_KEY = "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";
-AI_Template =
-  "Q: should i ask my crush on a date?/nA: no, what is there to like about you/n/nQ: am i attractive?/nA: no, deal with it/n/nQ: does my crush think im cute?/nA: no she actually thinks you are ugly/n/nQ: do my friends like me?/nA: yes, they think you are awesome/n/nQ: am i a cool person?/nA: no, you are extremely lame/n/nQ: do people care about me?/nA: definitely not/n/n";
-
-module.exports = { API_KEY, AI_Template };
